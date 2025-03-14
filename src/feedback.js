9b228af2084c974adae5facef3c4da1134aaddc6
fredrickUdom <fredrickudom@gmail.com>
2025-03-14 21:14:56 +0100

    cleaning

--- src/feedback.js
+++ src/feedback.js
@@ -76,70 +76,6 @@ const createPost = async (post) => {
 };
 
 
-// const createPost = async (post) => {
-//   try {
-    
-//     let body = typeof post === "string" ? JSON.parse(post) : post;
-
-//     const rating = body.rating;
-//     if (rating === undefined || rating < 1 || rating > 5) {
-//       throw new Error("Rating must be a number between 1 and 5.");
-//     }
-
-//     const predefinedAnswers = body.predefinedAnswers || [];
-//     if (!Array.isArray(predefinedAnswers)) {
-//       throw new Error("predefinedAnswers must be an array");
-//     }
-
-//     predefinedAnswers.forEach((question, index) => {
-//       if (!question.questionId || !question.question || !question.options) {
-//         throw new Error(`Invalid structure for question at index ${index}`);
-//       }
-//     });
-
-//     const id = uuidv4();
-//     const timestamp = new Date().toISOString();
-
-//     const params = {
-//       TableName: TABLE_NAME,
-//       Item: marshall({
-//         id,
-//         predefinedAnswers,
-//         customReason: body.customReason || "",
-//         rating: body.rating,
-//         createdAt: timestamp,
-//       }),
-//     };
-
-//     await db.send(new PutItemCommand(params));
-
-//     const getParams = {
-//       TableName: TABLE_NAME,
-//       Key: marshall({ id }),
-//     };
-
-//     const { Item } = await db.send(new GetItemCommand(getParams));
-
-//     // const unmarshalledItem = unmarshall(Item);
-
-//     return {
-//       statusCode: 201,
-//       body: JSON.stringify({
-//         message: "Successfully added feedback.",
-//         data: Item,
-//       }),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({
-//         error: error.message,
-//       }),
-//     };
-//   }
-// };
-
-
 const getAllPosts = async () => {
   const params = {
     TableName: TABLE_NAME,
@@ -229,10 +165,6 @@ const updatePostById = async (event) => {
         body: JSON.stringify({ error: "No valid fields to update." }),
       };
     }
-
-
-
-    // Update the item
 
     const params = {
       TableName: TABLE_NAME,
